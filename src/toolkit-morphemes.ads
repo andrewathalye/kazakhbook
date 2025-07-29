1f2ad85401505a04422e880b10d0047ff6947d52
Andrew Athalye <andrewathalye@outlook.com>
2025-07-29 09:36:30 -0400

    Update definitions (interim)

--- src/toolkit-morphemes.ads
+++ src/toolkit-morphemes.ads
@@ -12,43 +12,50 @@ package Toolkit.Morphemes is
    --  Stores data about known forms
    --  Linked to a Symbol_Database and a Feature_Database
 
-   type Morpheme is private;
-   --  A semantics-bearing information structure
-   --  This is equivalent to a morpheme
+   type Morpheme_Instance is private;
+   --  Stores the data required to determine the
+   --  pronunciation and features of a morph in
+   --  a given context
 
-   package Morpheme_Lists is new Ada.Containers.Vectors (Positive, Morpheme);
+   package Morpheme_Lists is new Ada.Containers.Vectors (Positive, Morpheme_Instance);
    subtype Morpheme_List is Morpheme_Lists.Vector;
+   --  A list of morpheme instances. This is the basis for a word.
+
+   type Abstract_Morpheme is private;
+   --  Stores data about meaning and features outside
+   --  of a morpheme context.
+
+   package Abstract_Morpheme_Lists is new Ada.Containers.Vectors (Positive, Abstract_Morpheme);
+   subtype Abstract_Morpheme is Abstract_Morpheme_Lists.Vector;
 
    -----------------
    -- Application --
    -----------------
    function Apply
-     (FODB     : Morpheme_Database; Input : Morpheme;
-      Features : Toolkit.Features.Feature_Set) return Morpheme;
+     (FODB     : Morpheme_Database; Input : Abstarct_Morpheme;
+      Features : Toolkit.Features.Feature_Set) return Abstract_Morpheme;
    --  Return the first Morpheme that can apply 'Features' to 'Input'
 
    function Apply
-     (FODB    : Morpheme_Database; Input : Morpheme;
+     (FODB    : Morpheme_Database; Input : Abstract_Morpheme;
       Symbols : Toolkit.Symbols.Abstract_Symbol_List) return Abstract_Morpheme;
    --  Return the first Morpheme matching 'Text' that can apply to 'Input'
 
-   function Assemble (ML : Morpheme_List) return Symbols.Abstract_Symbol_List;
-   --  Return the symbols associated with a list of Morphemes,
-   --  in writing / pronunciation order
-   --  TODO do better and return an annotated data set showing
-   --  where the Morphemes are?
+   function Resolve (ML : Abstract_Morpheme_List) return Morpheme_List;
+   --  Determine the necessary morphs, given a list of abstract morphemes
+   --  and context.
 
    ----------------
    -- Conversion --
    ----------------
-   function To_Symbols (M : Morpheme) return Symbols.Abstract_Symbol_List;
-   --  Return the symbols associated with a Morpheme as a list
+   function To_Symbols (M : Morpheme_Instance) return Symbols.Abstract_Symbol_List;
+   --  Return the symbols associated with a morph as a list
    --  Discontinous Morphemes are represented using Null_Abstract_Symbol
 
    function To_Morphemes
-     (MDB : Morpheme_Database; ASL : Toolkit.Symbols.Abstract_Symbol_List)
+     (MDB : Morpheme_Database; ASL : Toolkit.Symbols.Symbol_List)
       return Morpheme_List;
-   --  Return a best attempt at parsing an abstract symbol list into
+   --  Return a best attempt at parsing a symbol list into
    --  morphemes
 
    function To_XML (M : Morpheme) return String;
@@ -63,20 +70,33 @@ package Toolkit.Morphemes is
    --  Read all Morpheme definitions from an XML document
 private
    type Morpheme_Kind is
-     (Prefix, Stem, Suffix, Postfix, Circumfix, Triliteral, Transfix);
-   --  TODO handle duplifixes, interfixes, and infixes
+     (Prefix, Stem, Suffix, Postfix, Circumfix, Interfix, Triliteral, Transfix);
+   --  TODO handle duplifixes, and infixes
+
+   type Morph is record
+      Contexts : Toolkit.Contexts.Context_List;
+      Symbols : Toolkit.Symbols.Symbol_List;
+   end record;
+
+   package Morph_Lists is new Ada.Containers.Vectors (Natural, Morph);
+   subtype Morph_List is Morph_Lists.Vector;
 
    type Morpheme_Definition is record
       Contexts : Toolkit.Contexts.Context_List;
-      Symbols  : Toolkit.Symbols.Abstract_Symbol_List;
-      Features : Toolkit.Features.Feature_Set;
+      Morphs   : Morph_List;
       Kind     : Morpheme_Kind;
+      Features : Toolkit.Features.Feature_Set;
    end record;
 
    package MDL is new Ada.Containers.Vectors (Natural, Morpheme_Definition);
    type Morpheme_Database is new MDL.Vector with null record;
 
-   type Morpheme is record
-      C : FDL.Cursor;
+   type Abstract_Morpheme is record
+      C : MDL.Cursor;
+   end record;
+
+   type Morpheme_Instance is record
+      Morpheme : MDL.Cursor;
+      Instance : Morph_Lists.Cursor;
    end record;
 end Toolkit.Morphemes;
